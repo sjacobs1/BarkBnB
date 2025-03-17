ee4e7352db154cd47600ea668ecf0e65f65a305a
sebastianj7 <sebastianjacobs2000@gmail.com>
2025-03-17 12:19:08 +0200

    feat: :sparkles: user profile page
    added user navigation and profile page skeleton with user basic info: name, email, cell nr

--- app/(user)/profile.tsx
+++ app/(user)/profile.tsx
@@ -1,13 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from "react-native";
+import React from "react";
+import UserProfileCard from "../../components/user/profileComponents/userProfile/userProfileCard";
+import PetProfileCard from "../../components/user/profileComponents/petProfile/petProfileCard";
+import { useUserStore } from "../../hooks/UserStore";
+import style from "../../pageStyleSheets/profileStyleSheet";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {formatCellphoneNumber} from "../../utils/formatCellphoneNumber";
 
 const Profile = () => {
+  const userFirstName = useUserStore((state) => state.user?.firstName);
+  const userEmailAddress = useUserStore((state) => state.user?.email);
+  const userCellphoneNumber = useUserStore((state) => state.user?.cellNumber);
 
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
-  )
-}
+    <ScrollView style={style.mainContainer}>
+      <View>
+        <UserProfileCard name={userFirstName} email={userEmailAddress} cellNumber={formatCellphoneNumber(userCellphoneNumber)}/>
+        {/* <PetProfileCard /> */}
+      </View>
+    </ScrollView>
+  );
+};
 
-export default Profile
+export default Profile;
---
+++ pageStyleSheets/profileStyleSheet.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+const style = StyleSheet.create({
+    mainContainer: {
+        backgroundColor: "#F5F4F4",
+        padding: 10
+    }
+});
+
+export default style
