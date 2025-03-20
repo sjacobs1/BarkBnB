0c17189bf1d5c75fce8bae5cbc0105bc0e3eed7b
sebastianj7 <sebastianjacobs2000@gmail.com>
2025-03-20 10:23:35 +0200

    style: :lipstick: profile page style update
    updated user profile page style and moved logout to profile page

--- app/(user)/index.tsx
+++ app/(user)/index.tsx
@@ -1,27 +1,9 @@
 import { View, Text, Button } from "react-native";
-import { useAuth } from "../../AuthProvider";
-import { useRouter } from "expo-router";
-import { useUserStore } from "../../hooks/UserStore";
 
 const UserHome = () => {
-  const { logout } = useAuth();
-  const { user } = useUserStore();
-  console.log("Role:", user?.role);
-  const router = useRouter();
-
-  const navigateLogin = () => {
-    router.push("/(auth)/login");
-  };
-
-  const userSignOut = async () => {
-    await logout();
-    navigateLogin();
-  };
-
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>User Home Page</Text>
-      <Button title="Logout" onPress={userSignOut} />
     </View>
   );
 };
--- components/user/profileComponents/userProfile/userProfileCard.tsx
+++ components/user/profileComponents/userProfile/userProfileCard.tsx
@@ -1,8 +1,10 @@
-import { View, Text } from "react-native";
+import { View, Text, Touchable, TouchableOpacity } from "react-native";
 import React from "react";
 import style from "./userProfileCardStyleSheet";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
-import { LinearGradient } from "expo-linear-gradient";
+import { router, useRouter } from "expo-router";
+import { useAuth } from "../../../../AuthProvider";
+import { useUserStore } from "../../../../hooks/UserStore";
 
 interface userProfileCardProps {
   name: string | undefined;
@@ -11,16 +13,31 @@ interface userProfileCardProps {
 }
 
 const UserProfileCard = ({ name, email, cellNumber }: userProfileCardProps) => {
+  const { logout } = useAuth();
+  const { user } = useUserStore();
+  console.log("Role:", user?.role);
+  const router = useRouter();
+
+  const navigateLogin = () => {
+    router.push("/(auth)/login");
+  };
+
+  const userSignOut = async () => {
+    await logout();
+    navigateLogin();
+  };
+
   return (
-    <LinearGradient
-      colors={["white", "white"]}
-      start={[0.5, 0.1]}
-      locations={[0, 0.7]}
-      style={style.mainContainer}
-    >
-      <View style={style.nameContainer}>
-        <Text style={style.nameText}>{name}</Text>
+    <View style={style.mainContainer}>
+      <View style={style.nameAndEditContainer}>
+        <View style={style.editIconContainer}>
+          <MaterialIcons name="edit" size={24} color="#A4D4DF" />
+        </View>
+        <View style={style.nameContainer}>
+          <Text style={style.nameText}>{name}</Text>
+        </View>
       </View>
+
       <View style={style.detailsGroupContainer}>
         <View style={style.detailsContainer}>
           <View style={style.iconContainer}>
@@ -35,7 +52,13 @@ const UserProfileCard = ({ name, email, cellNumber }: userProfileCardProps) => {
           <Text style={style.userContactText}>{cellNumber}</Text>
         </View>
       </View>
-    </LinearGradient>
+
+      <View style={style.logoutContainer}>
+        <TouchableOpacity style={style.logoutButton} onPress={userSignOut}>
+          <Text style={style.logoutText}>Logout</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 };
 
