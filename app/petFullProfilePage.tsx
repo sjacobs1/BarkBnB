0cad2ce5c7d634dbdcdec230b6a2b15e666ff7bc
sebastianj7 <sebastianjacobs2000@gmail.com>
2025-04-14 15:10:12 +0200

    feat: :sparkles: pet full profile
    pet full profile page when user clicks on pet card

--- app/petFullProfilePage.tsx
+++ app/petFullProfilePage.tsx
@@ -1,10 +1,35 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { usePetStore } from '../hooks/petStore';
+import {getAge} from '../utils/getPetAge';
 
 const PetFullProfilePage = () => {
+  const selectedPet = usePetStore((state) => state.selectedPet);
+
+  if (!selectedPet) {
+    return (
+      <View>
+        <Text>No pet selected.</Text>
+      </View>
+    );
+  }
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Pet Full Profile Page</Text>
+      <Image source={{ uri: selectedPet.image }} />
+      <Text>{selectedPet.name}</Text>
+      <Text>Breed: {selectedPet.breed}</Text>
+      <Text>Gender: {selectedPet.gender}</Text>
+      <Text>
+        Birthdate:{" "}
+        {selectedPet.birthdate
+          ? `${getAge(selectedPet.birthdate!)?.years || 0} years, ${getAge(selectedPet.birthdate!)?.months || 0} months`
+          : "Unknown"}
+      </Text>
+      <Text>Vaccinated: {selectedPet.vaccine_status}</Text>
+      <Text>Neutered: {selectedPet.neutered ? "Yes" : "No"}</Text>
+      <Text>Dietary Requirements: {selectedPet.dietary_requirements}</Text>
+      <Text>Medical Requirements: {selectedPet.medical_requirements}</Text>
     </View>
   )
 }
--- hooks/petStore.ts
+++ hooks/petStore.ts
@@ -1,15 +1,29 @@
 import { create } from "zustand";
-import { Pet } from "../app/models/pet";
-import { useGetPetsQuery } from "../app/services/pet/petSlice";
+
+interface Pet {
+  name: string;
+  image?: string;
+  breed?: string;
+  gender?: string;
+  birthdate?: Date;
+  vaccine_status?: string;
+  neutered?: boolean;
+  dietary_requirements?: string;
+  medical_requirements?: string;
+}
 
 interface PetStore {
   pets: Pet[];
   addPet: (pet: Pet) => void;
   setPets: (pets: Pet[]) => void;
+  selectedPet: Pet | null;
+  setSelectedPet: (pet: Pet | null) => void;
 }
 
 export const usePetStore = create<PetStore>((set) => ({
   pets: [],
   addPet: (pet: Pet) => set((state) => ({ pets: [...state.pets, pet] })),
   setPets: (pets: Pet[]) => set(() => ({ pets })),
+  selectedPet: null,
+  setSelectedPet: (pet) => set({ selectedPet: pet }),
 }));
